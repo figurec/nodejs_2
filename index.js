3c0b5fb24483e0a66f407ef8e13ec6d266a9e561
figurec <86502121+figurec@users.noreply.github.com>
2021-06-26 07:12:57 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const net = require('net');
+const { URL } = require('url');
+
+const PORT = process.env.PORT || 5000;
+
+// Create an HTTP tunneling proxy
+const proxy = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('okay');
+});
+proxy.on('connect', (req, clientSocket, head) => {
+  // Connect to an origin server
+  const { port, hostname } = new URL(`http://${req.url}`);
+  const serverSocket = net.connect(port || 80, hostname, () => {
+    clientSocket.write('HTTP/1.1 200 Connection Established\r\n' +
+                    'Proxy-agent: Node.js-Proxy\r\n' +
+                    '\r\n');
+    serverSocket.write(head);
+    serverSocket.pipe(clientSocket);
+    clientSocket.pipe(serverSocket);
+  });
+});
+
+proxy.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
+});
