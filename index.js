e30da90a56bfdb0621be1458de6f0e35f8e72d0f
figurec <86502121+figurec@users.noreply.github.com>
2021-06-26 10:04:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const proxy = http.createServer((req, res) => {
 proxy.on('connect', (req, clientSocket, head) => {
   // Connect to an origin server
   const { port, hostname } = new URL(`http://${req.url}`);
+  console.log("hostname: " + hostname);
+  console.log("port: " + port);
   const serverSocket = net.connect(port || 80, hostname, () => {
     clientSocket.write('HTTP/1.1 200 Connection Established\r\n' +
                     'Proxy-agent: Node.js-Proxy\r\n' +
